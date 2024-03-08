6b7ae946aeba5042c2412c7c233eed10f0c47152
Grigory Shepelev <shegeley@gmail.com>
2024-03-08 17:33:22 +0300

    repl.sh+readme.org: geiser->arei+ares

--- repl.sh
+++ repl.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-guix shell guile -f guix.scm --rebuild-cache -- guile --listen=1338
+# # geiser
+# guix shell guile -f guix.scm \
+#      --rebuild-cache --development \
+#      -- guile \
+#      --listen=1338
+
+# ares + arei
+guix shell guile-next guile-ares-rs -f guix.scm \
+     --rebuild-cache --development \
+     -- guile \
+     -c '((@ (nrepl server) run-nrepl-server) #:port 7888)'
