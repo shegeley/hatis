e2280711224e185b6a65b7ff76ada0de7a7a8e75
Grigory Shepelev <shegeley@gmail.com>
2024-03-13 16:57:38 +0300

    (wayland keyboard): +comment

--- src/hatis/wayland/keyboard.scm
+++ src/hatis/wayland/keyboard.scm
@@ -15,9 +15,11 @@
   (match format
     (0 'no-keymap)
     (1
+     ;; can be rewritten using: ~fdopen~ + ~(ice-9 binary-ports)~
+     ;; https://www.gnu.org/software/guile/manual/html_node/Binary-I_002fO.html
      (let* [(bytevector-keymap (mmap
                                 #f size
-                                (logior PROT_READ PROT_WRITE)
+                                PROT_READ
                                 MAP_PRIVATE fd 0))
             ;; TODO: munmap? memory safety?
             (keymap (utf8->string bytevector-keymap))]
