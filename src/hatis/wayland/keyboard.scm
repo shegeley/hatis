cd1886c963922cdc21d09163c65908e89d5e061f
Grigory Shepelev <shegeley@gmail.com>
2024-03-13 08:06:53 +0300

    (hatis wayland keyboard): better ~get-keyboard~

--- src/hatis/wayland/keyboard.scm
+++ src/hatis/wayland/keyboard.scm
@@ -2,10 +2,10 @@
 
   #:use-module ((ice-9 format)
                 #:select ((format . format*)))
-
   #:use-module (ice-9 match)
 
-  #:use-module (scheme base)
+  #:use-module (rnrs bytevectors)
+
   #:use-module (mmap)
 
   #:export (get-keymap))
@@ -13,12 +13,13 @@
 (define (get-keymap format fd size)
   (format* #t "get-keymap-args: ~a ~a ~a" format fd size)
   (match format
-    (0  ;; do nothing?
-     'no-keymap)
-    (1 ;; should return bytevector
-     (let [(keymap (mmap
-                    #f size
-                    (logior PROT_READ PROT_WRITE)
-                    MAP_PRIVATE fd 0))]
-       ;; TODO: munmap? memory safety?
-       (format* #t "keymap: ~a ~%" keymap)))))
+    (0 'no-keymap)
+    (1
+     (let* [(bytevector-keymap (mmap
+                                #f size
+                                (logior PROT_READ PROT_WRITE)
+                                MAP_PRIVATE fd 0))
+            ;; TODO: munmap? memory safety?
+            (keymap (utf8->string bytevector-keymap))]
+       (format* #t "keymap: ~a ~%" keymap)
+       keymap))))
