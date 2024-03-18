c370bdf5461d699d84542a5351e644ce3d019709
Grigory Shepelev <shegeley@gmail.com>
2024-03-18 12:52:33 +0300

    (hatis wayland): parameters -> atomic-box

--- src/hatis/wayland.scm
+++ src/hatis/wayland.scm
@@ -16,8 +16,20 @@
   #:use-module (ice-9 match)
   #:use-module (ice-9 format)
   #:use-module (ice-9 threads)
+  #:use-module (ice-9 atomic)
 
   #:use-module (oop goops))
+
+;; clojure-alike atomic-box interfaces
+(define (update x f)
+  (atomic-box-set! x (f (atomic-box-ref x))))
+
+(define (reset! x val)
+  (atomic-box-set! x val))
+
+(define (ref x)
+  (atomic-box-ref x))
+;; end
 
 (define (current-desktop)
   (getenv "XDG_CURRENT_DESKTOP"))
@@ -26,37 +38,40 @@
   (make-channel))
 
 (define compositor
-  (make-parameter #f))
+  (make-atomic-box #f))
 
 (define display
-  (make-parameter #f))
+  (make-atomic-box #f))
 
 (define registry
-  (make-parameter #f))
+  (make-atomic-box #f))
 
 (define seat
-  (make-parameter #f))
+  (make-atomic-box #f))
 
 (define input-method-manager
-  (make-parameter #f))
+  (make-atomic-box #f))
 
 (define input-method
-  (make-parameter #f))
+  (make-atomic-box #f))
 
 (define input-surface
   ;; https://wayland.app/protocols/input-method-unstable-v2#zwp_input_popup_surface_v2
-  (make-parameter #f))
+  (make-atomic-box #f))
 
 (define xdg-input-surface
   ;; https://wayland.app/protocols/xdg-shell#xdg_surface
-  (make-parameter #f))
+  (make-atomic-box #f))
 
 (define keyboard
-  (make-parameter #f))
+  (make-atomic-box #f))
+
+(define keymap
+  (make-atomic-box #f))
 
 (define xdg-wm-base
   ;; The xdg_wm_base interface is exposed as a global object enabling clients to turn their wl_surfaces into windows in a desktop environment. It defines the basic functionality needed for clients and the compositor to create windows that can be dragged, resized, maximized, etc, as well as creating transient windows such as popup menus.
-(make-parameter #f))
+(make-atomic-box #f))
 
 (define registry-listener
   (make <wl-registry-listener>
@@ -66,18 +81,16 @@
               interface version name)
       (cond
        ((string=? "wl_compositor" interface)
-        (compositor
-         (wrap-wl-compositor (wl-registry-bind registry name %wl-compositor-interface 3))))
+        (reset! compositor (wrap-wl-compositor (wl-registry-bind registry name %wl-compositor-interface 3))))
        ((string=? "wl_seat" interface)
-        (seat (wrap-wl-seat (wl-registry-bind registry name %wl-seat-interface 3))))
+        (reset! seat (wrap-wl-seat (wl-registry-bind registry name %wl-seat-interface 3))))
        ((string=? "zwp_input_method_manager_v2" interface)
-        (input-method-manager
-         (wrap-zwp-input-method-manager-v2
-          (wl-registry-bind registry name %zwp-input-method-manager-v2-interface 1))))
+        (reset! input-method-manager (wrap-zwp-input-method-manager-v2
+                                      (wl-registry-bind registry name %zwp-input-method-manager-v2-interface 1))))
        ((string=? "xdg_wm_base" interface)
-        (xdg-wm-base
-         (wrap-xdg-wm-base
-          (wl-registry-bind registry name %xdg-wm-base-interface 2))))))
+        (reset! xdg-wm-base
+                (wrap-xdg-wm-base
+                 (wl-registry-bind registry name %xdg-wm-base-interface 2))))))
     #:global-remove
     (lambda (data registry name)
       (pk 'remove data registry name))))
@@ -91,7 +104,8 @@
                             (key . ,key)
                             (state . ,state))))"
   (format #t "key! args: ~a ~%" args)
-  (put-message chan (list #:key args)))
+  ;; (put-message chan (list #:key args))
+  )
 
 (define keyboard-grab-listener
   (make <zwp-input-method-keyboard-grab-v2-listener>
@@ -101,7 +115,7 @@
     #:keymap
     (lambda args
       (format #t "keymap! args: ~a ~%" args)
-      (apply get-keymap (drop args 2)))
+      (reset! keymap (apply get-keymap (drop args 2))))
     #:modifiers
     (lambda args
       (format #t "modifiers! args: ~a ~%" args))
@@ -136,18 +150,18 @@
       ;; NOTE: need to grab keyboard + input surface
 
       ;; Catch surface
-      (input-surface
-       (wl-compositor-create-surface (compositor)))
+      (reset! input-surface
+       (wl-compositor-create-surface (ref compositor)))
 
-      (input-surface
+      (reset! input-surface
        ;; popup-input-surface won't cast to xdg-surface
        ;; (xdg-input-surface (xdg-wm-base-get-xdg-surface (xdg-wm-base) (input-surface)))
        ;; ERROR: «not a <wl-surface> or #f #<<zwp-input-popup-surface-v2> 7efd12918c80>»
-       (zwp-input-method-v2-get-input-popup-surface im (input-surface)))
+       (zwp-input-method-v2-get-input-popup-surface im (ref input-surface)))
 
       ;; Grab keyboard
-      (keyboard (zwp-input-method-v2-grab-keyboard im))
-      (zwp-input-method-keyboard-grab-v2-add-listener (keyboard) keyboard-grab-listener))
+      (reset! keyboard (zwp-input-method-v2-grab-keyboard im))
+      (zwp-input-method-keyboard-grab-v2-add-listener (ref keyboard) keyboard-grab-listener))
     #:deactivate
     (lambda args
       (format #t "leave! args: ~a ~%" args)
@@ -156,38 +170,39 @@
       )))
 
 (define (main)
-  (display (wl-display-connect))
-  (unless (display)
+  (reset! display (wl-display-connect))
+  (unless (ref display)
     (display "Unable to connect to wayland compositor")
     (newline)
     (exit -1))
-  (format #t "Connect to Wayland compositor: ~a ~%" (display))
-  (registry (wl-display-get-registry (display)))
-  (wl-registry-add-listener (registry) registry-listener)
-  (wl-display-roundtrip (display))
-  (if (input-method-manager)
+  (format #t "Connect to Wayland compositor: ~a ~%" (ref display))
+  (reset! registry (wl-display-get-registry (ref display)))
+  (wl-registry-add-listener (ref registry) registry-listener)
+  (wl-display-roundtrip (ref display))
+  (if (ref input-method-manager)
       (format #t "Got it!~%")
       (error (format #f "Can't access input-manager!")))
-  (format #t "Input-method manager: ~a ~%" (input-method-manager))
-  (input-method (zwp-input-method-manager-v2-get-input-method
-                 (input-method-manager)
-                 (seat)))
-  (format #t "Input-method: ~a ~%" (input-method))
-  (zwp-input-method-v2-add-listener (input-method) input-method-listener)
-  (while (wl-display-roundtrip (display))))
+  (format #t "Input-method manager: ~a ~%" (ref input-method-manager))
+  (reset! input-method (zwp-input-method-manager-v2-get-input-method
+                 (ref input-method-manager)
+                 (ref seat)))
+  (format #t "Input-method: ~a ~%" (ref input-method))
+  (zwp-input-method-v2-add-listener (ref input-method) input-method-listener)
+  (while (wl-display-roundtrip (ref display))))
 
-;; Evaluate (main) only AFTER evaluating everything before it. Or ~arei~ buffer might loose the (current-output-(port?)) and won't show any print commands
-;; (main)
+(define output-file
+  (make-parameter "./output.txt"))
 
-;; (use-modules (ice-9 textual-ports))
-;; TODO: parameters => (ice-9 atomics)?
+(define (reset-output)
+  (when (file-exists? (output-file))
+    (delete-file (output-file))))
+
+(define output-port
+  (open-file (output-file) "a0"))
 
 (define thread
   (call-with-new-thread
    (lambda ()
-     (with-output-to-file "./output.txt"
-       (lambda () (main))))))
+     (with-output-to-port output-port main))))
 
-;; (cancel-thread thread) <- stop
-
-;; use system-async-mark on the thread (need to create continuation first) <- pause
+;; (cancel-thread thread)
