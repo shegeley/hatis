478e83a17fd4d11c4c827228a132ed3e7fc6a443
Grigory Shepelev <shegeley@gmail.com>
2024-04-02 10:12:26 +0300

    (hatis sway): + versioning table

---
+++ src/hatis/sway.scm
@@ -0,0 +1,15 @@
+(define-module (hatis sway)
+
+  #:use-module (srfi srfi-69) ;; hash-tables
+
+  #:export (versioning))
+
+;; Maybe it's a bad idea to directly keep knowledge of sway's wayland interfaces versioning inside hatis...
+;; It would be better to teach guile-wayland read versions directly from XMLs (why it won't do so???)
+
+(define versioning
+  (alist->hash-table
+   `((wl-compositor . 3)
+     (wl-seat . 3)
+     (zwp-input-manager-v2 . 1)
+     (xdg-wm-base . 2))))
