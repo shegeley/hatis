c1cb88d0dade501d57ec5a652cc585ccdedc2684
Grigory Shepelev <shegeley@gmail.com>
2024-04-04 12:51:11 +0300

    (hatis utils): + _->-, live-load

--- src/hatis/utils.scm
+++ src/hatis/utils.scm
@@ -1,9 +1,20 @@
 (define-module (hatis utils)
   #:use-module (rnrs bytevectors)
+
+  #:use-module (ice-9 string-fun)
   #:use-module (ice-9 binary-ports)
 
-  #:export (read-string-from-fd))
+  #:export (read-string-from-fd _->- live-load))
 
 (define (read-string-from-fd fd)
   (call-with-port (fdopen fd "rb")
     (compose utf8->string get-bytevector-all)))
+
+(define (_->- str)
+   (string-replace-substring str "_" "-"))
+
+(define* (live-load x #:key (module (current-module)))
+  (module-ref module
+              (cond
+               ((string? x) string->symbol)
+               ((symbol? x) x)) x))
