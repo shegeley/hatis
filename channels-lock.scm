4345b50882d65c2883bf5de2383d7657abac2624
Grigory Shepelev <shegeley@gmail.com>
2024-05-02 06:31:09 +0300

    + commit-lock.scm

---
+++ channels-lock.scm
@@ -0,0 +1,10 @@
+(list
+ (channel
+  (name 'guix)
+  (url "https://git.savannah.gnu.org/git/guix.git")
+  (commit "4115590ed1f4a95e52a687d0d0a4bab69e18f4d7")
+  (introduction
+   (make-channel-introduction
+    "9edb3f66fd807b096b48283debdcddccfea34bad"
+    (openpgp-fingerprint
+     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
