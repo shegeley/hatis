5a01eceeddb5a6e09fcc5627ba0541fb3b2ea95d
Grigory Shepelev <shegeley@gmail.com>
2024-06-01 14:31:13 +0300

    (guix packages clojureism): srfi-69->srfi-125 [typo fix]

--- guix/packages/clojureism.scm
+++ guix/packages/clojureism.scm
@@ -29,7 +29,7 @@
    (native-inputs (list guile-3.0))
    (synopsis "Small guile scheme libriary to provide clojure-alike atom, vector and hash-map basic procedures")
    (description "The point of this small libriary is to mimic clojure's basic operations on data structures with guile scheme as close as possible.
-Structures correspondence: vector [clojure] <-> vector [scheme], hash-map [clojure] <-> srfi-69 hash-table [scheme], atom [clojure] <-> ice-9 atomic [scheme].
+Structures correspondence: vector [clojure] <-> vector [scheme], hash-map [clojure] <-> srfi-125 hash-table [scheme], atom [clojure] <-> ice-9 atomic [scheme].
 Operations: get, get-in (vector, hash-table); assoc, assoc-in (vector, hash-table); update, update-in (vector, hash-table); ref, swap!, reset! (atomic).
 Bonus: clojure-alike hash-table printer.")
    (license license:gpl3+)
