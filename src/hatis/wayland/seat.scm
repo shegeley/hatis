f4cf708ed71c10fafccdc7baf3068275d26b2f99
Grigory Shepelev <shegeley@gmail.com>
2024-08-05 17:06:19 +0300

    (hatis wayland seat): `extract-capabilities` match -> hash-table

--- src/hatis/wayland/seat.scm
+++ src/hatis/wayland/seat.scm
@@ -1,17 +1,18 @@
 (define-module (hatis wayland seat)
-  #:use-module (ice-9 match)
-  #:use-module (ice-9 format)
+  #:use-module (srfi srfi-125)
 
   #:export (extract-capabilities))
 
-(define (extract-capabilities x)
-  ;; https://wayland.app/protocols/wayland#wl_seat:enum:capability
-  (match x
-    (7 '(pointer keyboard touch))
-    (6 '(keyboard touch))
-    (5 '(pointer touch))
-    (4 '(touch))
-    (3 '(keyboard pointer))
-    (2 '(keyboard))
-    (1 '(pointer))
-    (else (error (format #f "Can't parse ~a ~%" x)))))
+(define capabilities-table
+  (alist->hash-table
+   `((7 . (pointer keyboard touch))
+     (6 . (keyboard touch))
+     (5 . (pointer touch))
+     (4 . (touch))
+     (3 . (keyboard pointer))
+     (2 . (keyboard))
+     (1 . (pointer))) eqv?))
+
+(define (extract-capabilities value)
+  "https://wayland.app/protocols/wayland#wl_seat:enum:capability"
+  (hash-table-ref/default capabilities-table value #f))
