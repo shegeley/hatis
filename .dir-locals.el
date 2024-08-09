fbe4cb46e4b7e05a593911d3abc8c63e30eecdda
Grigory Shepelev <shegeley@gmail.com>
2024-08-09 22:14:50 +0300

    .dir-locals.el: "arei" don't `kill-buffer` on disconnect

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,11 @@
 ((scheme-mode
   .
-  ((geiser-mode-auto-p . nil))))
+   ((geiser-mode-auto-p . nil)
+    (indent-tabs-mode . nil)
+    (eval . (setq lisp-indent-offset 1))
+    (eval . (eval-after-load "arei"
+             '(defun arei--sentinel (process message)
+               "Called when connection is changed; in out case dropped."
+               (message "nREPL connection closed: %s" message)
+               ;; (kill-buffer (process-buffer process))
+               ))))))
