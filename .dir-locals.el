0a0575aebf7f7be014b495ec7f355f408c30b146
Grigory Shepelev <shegeley@gmail.com>
2024-09-01 14:25:39 +0300

    .dir-locals.el: `arei-client--sentiel` don't kill on process exit

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,7 +4,8 @@
     (indent-tabs-mode . nil)
     (eval . (setq lisp-indent-offset 1))
     (eval . (eval-after-load "arei"
-             '(defun arei--sentinel (process message)
+             ;; don't kill arei buffer when the process is dead; when dealing with SIGSEGV there might be some useful prints
+             '(defun arei-client--sentinel (process message)
                "Called when connection is changed; in out case dropped."
                (message "nREPL connection closed: %s" message)
                ;; (kill-buffer (process-buffer process))
