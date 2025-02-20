a8f8bab8498868404e5690970a321c996dc7e008
Grigory Shepelev <shegeley@gmail.com>
2025-02-20 22:31:23 +0300

    hatis.draft: CLOS-based

--- src/hatis/core.lisp
+++ src/hatis/core.lisp
@@ -19,17 +19,7 @@
 
 (in-package :xyz.hatis.core)
 
-(defparameter *channel* (make-instance 'channel))
-
-(defparameter *display* nil)
-
-(defun get-interface (type)
-  "keep in mind that there can be multiple interfaces of the same time in display's proxy-table. find-if only returns first"
- (->>
-   '%proxy-table
-   (slot-value *display*)
-   (a:hash-table-values)
-   (find-if (lambda (x) (eq type (type-of x))))))
+(defparameter app nil)
 
 (defparameter key->code
  `((Esc . 1)))
@@ -42,122 +32,157 @@
    zwp-input-method-manager-v2
    zwlr-foreign-toplevel-manager-v1))
 
-(defmethod handle-interface-event (i e) (lambda (&rest args) args))
+(defclass Hatis ()
+ ((display :type wl-display
+   :initarg :display
+   :accessor %display)
+  (channel :type channel
+   :initarg :channel
+   :accessor %channel)))
+
+(defmethod get-interface
+ ((display wl-display) type)
+ (->>
+  '%proxy-table
+  (slot-value display) (a:hash-table-values)
+  (find-if (lambda (x) (eq type (type-of x))))))
+
+(defmethod get-interface
+ ((H Hatis) type) (get-interface (%display H) type))
+
+(defmethod get-input-method ((display wl-display))
+ (let* ((gim  #'zwp-input-method-manager-v2.get-input-method)
+        (imm  (get-interface display 'zwp-input-method-manager-v2))
+        (im   (get-interface display 'zwp-input-method-v2))
+        (seat (get-interface display 'wl-seat)))
+  (cond
+   (im im)
+   ((and imm seat) (funcall gim imm seat))
+   (t (signal 'cannot-get-input-method)))))
+
+(defmethod get-input-method ((H Hatis))
+ (with-slots (display) H (get-input-method display)))
+
+(defun bind (registry id interface version)
+ (wl-registry.bind registry id
+  (interface-string->symbol interface) version))
+
+(defmethod handle-interface-event (h i e) (lambda (&rest args) args))
 
 (defmethod handle-interface-event
- ((i zwlr-foreign-toplevel-manager-v1) (e (eql :toplevel)))
- (lambda (handle) (process-interface handle)))
+ (_ (i zwlr-foreign-toplevel-manager-v1) (e (eql :toplevel)))
+ (lambda (handle) handle))
 
 (defmethod handle-interface-event
- ((i zwp-input-method-keyboard-grab-v2) (e (eql :keymap)))
+ (_ (i zwp-input-method-keyboard-grab-v2) (e (eql :keymap)))
  (lambda (&rest args) ;; (serial time key state)
   (cons :keymap args)))
 
 (defmethod handle-interface-event
- ((i zwp-input-method-keyboard-grab-v2) (e (eql :key)))
+ ((H Hatis) (i zwp-input-method-keyboard-grab-v2) (e (eql :key)))
  (lambda (serial time keycode state)
  (cond
    ((= (assoc-ref key->code `Esc) keycode)
-    (let ((imkg (get-interface 'zwp-input-method-keyboard-grab-v2)))
-     (format t "Releasing keyboard grab ~a ~%" imkg)
+    (let ((imkg (get-interface H 'zwp-input-method-keyboard-grab-v2)))
      (zwp-input-method-keyboard-grab-v2.release imkg)))
   (t (list serial time keycode state)))))
 
 (defmethod handle-interface-event
- ((i zwp-input-method-keyboard-grab-v2) (e (eql :modifiers)))
- (lambda (&rest args)
-  ;; (serial time key state)
+ (_ (i zwp-input-method-keyboard-grab-v2) (e (eql :modifiers)))
+ (lambda (&rest args) ;; (serial time key state)
   (cons :modifiers args)))
 
 (defmethod handle-interface-event
- ((i zwp-input-method-v2) (e (eql :activate)))
+ ((H hatis) (i zwp-input-method-v2) (e (eql :activate)))
  (lambda ()
   (let ((grab (zwp-input-method-v2.grab-keyboard i)))
-   (process-interface grab))))
+   (process-interface H grab))))
 
 (defmethod handle-interface-event
- ((i zwp-input-method-v2) (e (eql :deactivate)))
+ ((H Hatis) (i zwp-input-method-v2) (e (eql :deactivate)))
  (lambda ()
-  (let ((grab (get-interface 'zwp-input-method-keyboard-grab-v2)))
+  (let ((grab (get-interface H 'zwp-input-method-keyboard-grab-v2)))
    (when grab
     (zwp-input-method-keyboard-grab-v2.release grab)
     (list 'deactivated 'zwp-input-method-keyboard-grab-v2)))))
 
 (defmethod handle-interface-event
-    ((i zwp-input-method-v2) (e (eql :content-type)))
+ (_ (i zwp-input-method-v2) (e (eql :content-type)))
   ;; EXAMPLE (CONTENT-TYPE (NONE) TERMINAL)
   (lambda (_ type)
-    (cond ((eql type :terminal)
-           "do something given it's a terminal"))))
+   (cond
+    ((eql type :terminal)
+     "do something given it's a terminal"))))
 
 (defmethod handle-interface-event
- ((registry wl-registry) (e (eql :global)))
+ ((H Hatis) (registry wl-registry) (e (eql :global)))
  (lambda (id interface version)
   (let ((sinterface (interface-string->symbol interface)))
    (if (find sinterface registry-global-interfaces-bind-list)
     (let ((interface-object (bind registry id interface version)))
-     (process-interface interface-object))
+     (process-interface H interface-object))
     (list 'wl-registry 'global (list id interface version))))))
 
 (defun handle-interface-event*
- (interface event &key (channel *channel*))
+ (H interface event)
  "Handle wayland's interface event according to the handle-interface-event method and send result to the channel"
- (destructuring-bind (event-name &rest event-args) event
-  (let ((r (apply (handle-interface-event interface event-name) event-args)))
-   (send channel (cons 'wayland-interface-event event))
-   (send channel (cons 'handled-event-result r)))))
+ (with-slots (channel) H
+  (destructuring-bind (event-name &rest event-args) event
+   (let ((r (apply (handle-interface-event H interface event-name)
+             event-args)))
+    (cond
+     ((typep r 'wl-proxy) (process-interface H r))
+     (t
+      (send channel (cons 'wayland-interface-event event))
+      (send channel (cons 'handled-event-result r))))))))
 
-(defun install-event-handlers! (interface)
+(defun install-event-handlers! (H interface)
  (push
-  (lambda (event) (handle-interface-event* interface event))
+  (lambda (event) (handle-interface-event* H interface event))
   (wl-proxy-hooks interface)))
 
-(defun bind (registry id interface version)
- (wl-registry.bind registry id (interface-string->symbol interface) version))
-
 (defmethod process-interface
- ((d wl-display))
-  (progn
-    (setq *display* d)
-    (process-interface (wl-display.get-registry *display*))
-    ;; double roundrtip needed to catch all the interfaces + toplevel manager&handle
-    (wl-display-roundtrip *display*)
-    (wl-display-roundtrip *display*)))
-
-(defmethod process-interface
- (interface)
- "This method is called BEFORE all the interfaces are 'collected' into %proxy-table. So you can't rely on it's being filled on this method's first call"
+ (H (d wl-display))
  (progn
-  (install-event-handlers! interface)
-  (list 'processed interface)))
+  (process-interface H (wl-display.get-registry d))
+  ;; double roundrtip needed to catch all the interfaces + toplevel manager&handle
+  (wl-display-roundtrip d) (wl-display-roundtrip d)))
 
-(defun get-input-method ()
-  (let* ((imm  (get-interface 'zwp-input-method-manager-v2))
-         (im   (get-interface 'zwp-input-method-v2))
-         (seat (get-interface 'wl-seat)))
-    (if im im (zwp-input-method-manager-v2.get-input-method imm seat))))
+(defmethod process-interface
+ (H interface)
+ "This method is called BEFORE all the interfaces are 'collected' into %proxy-table. So you can't rely on it's being filled on this method's first call"
+ (with-slots (channel) H
+  (format t "processing: ~a ~%" interface)
+  (send channel (list 'processing interface))
+  (install-event-handlers! H interface)
+  (list 'processed interface)))
 
 (defun start! ()
  (print "Starting hatis…") (terpri)
- (with-open-display (display)
-   (pexec () (loop (format t "~a~%" (recv *channel*))))
-   (process-interface display)
-   (process-interface (get-input-method))
-   (loop
-     (if (not (eql 'wl-destroyed-proxy (type-of *display*)))
-         (wl-display-dispatch-event *display*)
-         (return t)))))
+ (with-open-display (display*)
+  (let ((hatis (make-instance 'Hatis
+                :display display*
+                :channel (make-instance 'channel))))
+   (setq app hatis)
+   (with-slots (display channel) hatis
+    (pexec () (loop (format t "~a~%" (recv channel))))
+    (process-interface hatis display*)
+    (process-interface hatis (get-input-method hatis))
+    (loop
+     (if (not (eql 'wl-destroyed-proxy (type-of display)))
+      (wl-display-dispatch-event display)
+      (return t)))))))
 
 (defun stop! ()
  (handler-case
   (progn
    (print "Stopping hatis…") (terpri)
-   (wl-display.sync *display*)
-   (wl-display-disconnect *display*))
+   (wl-display.sync (%display app))
+   (wl-display-disconnect (%display app)))
   (error (c)
    (print "Coudn't stop hatis…") (terpri)
    (format *error-output* "Caught error: ~a ~%" c)
    nil)))
 
-;; (start!)
 ;; (stop!)
+;; (start!)
